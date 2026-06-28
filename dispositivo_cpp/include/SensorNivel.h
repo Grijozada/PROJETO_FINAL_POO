2e1e86d21fa7f037b4b05ea7e49e87891c077bcc
Gustavo Grijo <grijogustavo08@gmail.com>
2026-06-28 00:45:51 -0300

    Implementa falha simulada no sensor de nivel

--- dispositivo_cpp/include/SensorNivel.h
+++ dispositivo_cpp/include/SensorNivel.h
@@ -5,12 +5,44 @@
 
 class SensorNivel : public Sensor
 {
+private:
+    bool travado;
+    int ciclosTravado;
+    float valorTravado;
+
 public:
-    SensorNivel() : Sensor("NIVEL", "%") {}
+    SensorNivel()
+        : Sensor("NIVEL", "%"), travado(false), ciclosTravado(0), valorTravado(0) {}
+
+    void simularTravamento(int ciclos)
+    {
+        travado = true;
+        ciclosTravado = ciclos;
+        valorTravado = valor;
+        status = "TRAVADO";
+    }
 
     float ler() override
     {
+        if (travado && ciclosTravado > 0)
+        {
+            ciclosTravado--;
+
+            if (ciclosTravado == 0)
+            {
+                travado = false;
+                status = "NORMAL";
+            }
+
+            return valorTravado;
+        }
+
         return valor;
+    }
+
+    bool estaTravado() const
+    {
+        return travado;
     }
 };
 
